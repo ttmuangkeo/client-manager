eeaf75c4a16ec68cea297887c2a8db59a1e4c812
Tim Muangkeo <tim.muangkeo@moxiworks.com>
2024-01-29 23:44:44 -0800

    added a get request moxi companies and return it the front page

--- app/controllers/moxi_auth_controller.rb
+++ app/controllers/moxi_auth_controller.rb
@@ -1,14 +1,25 @@
 class MoxiAuthController < ApplicationController
-    def fetch_company_data
+    def authenticate
         provided_username = params[:username]
         provided_password = params[:password]
-
-        company_data_result = AuthenticationService.fetch_moxi_company_data(provided_username, provided_password)
-
-        if company_data_result[:success]
-            render json: {company_data: company_data_result[:company_data]}
+    
+        # Call a service or method to authenticate and fetch data from the external API
+        result = AuthenticationService.fetch_moxi_company_data(provided_username, provided_password)
+    
+        if result[:success]
+          render json: { data: result[:data] }
         else
-            render json: {error: 'failed to get fetch company data'}, status: :internal_server_error
+          render json: { error: 'Authentication failed' }, status: :unauthorized
+        end
+    end
+
+    def show
+        company_result = AuthenticationService.fetch_moxi_company_data
+
+        if company_result[:success]
+            render json: {company_result[:companies]}
+        else
+            render json: {error: 'failed to get comapny data'}, status: :internal_server_error
         end
     end
 end

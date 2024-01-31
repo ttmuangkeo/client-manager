24abd796bc79e0ea5989e2da2699abc6eb68fdd5
Tim Muangkeo <tim.muangkeo@moxiworks.com>
2024-01-30 17:50:35 -0800

    refactor moxi auth controller

--- app/assets/javascripts/src/services/AuthenticationService.js
+++ app/assets/javascripts/src/services/AuthenticationService.js
@@ -1,28 +1,34 @@
 import axios from "axios";
-const BASE_URL = "/moxi";
-const COMPANY_ENDPOINT = "/companies";
 
 async function authenticate(username, password) {
-    try {
-        const response = await axios.get(`${BASE_URL}${COMPANY_ENDPOINT}`, {
-            headers: {
-                Authorization: `Basic ${btoa(`${username}:${password}`)}`,
-                "Content-Type": 'application/x-www-form-urlencoded',
-                Accept: 'application/vnd.moxi-platform+json;version=1'
-            }
-        })
-        return {
-            success: true,
-            data: response.data
-        }
-    } catch(err) {
-        console.log('auth error', err);
-        return{
-            success: false,
-            error: 'failed to auth in'
-        };
-    }
+  console.log("from my auth service in vue", username, password);
+  try {
+    const response = await axios.post(
+      "moxi/authenticate",
+      {
+        username: username,
+        password: password,
+      },
+      {
+        withCredentials: true,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/vnd.moxi-platform+json;version=1",
+        },
+      }
+    );
+    console.log('response back ', response)
+    return {
+      success: true,
+      data: response.data,
+    };
+  } catch (err) {
+    console.log("auth error", err);
+    return {
+      success: false,
+      error: "failed to auth in",
+    };
+  }
 }
-
 
 export { authenticate };
